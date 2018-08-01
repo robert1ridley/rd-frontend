aaf2885bb097617e718560ca45d54b7df079d320
Robert Ridley <robert.n.ridley@gmail.com>
2018-08-01 23:27:07 +0800

    begin Login feature

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, KeyboardAvoidingView } from 'react-native';
-import { Form, Item, Input, Button, Text } from 'native-base';
-import { darkBlue, darkGreen, white } from '../helpers/colors';
-import { getWebToken } from '../helpers/utils';
+import { Form, Item, Input, Button, Text, View } from 'native-base';
+import { darkBlue, darkGreen, white, red } from '../helpers/colors';
+import { getWebToken, processResponse, setWebToken } from '../helpers/utils';
 import { BASE_URL } from 'react-native-dotenv';
 
 class Login extends React.Component {
+
+  state = {
+    user_name: '',
+    password: '',
+    userError: null,
+    passwordError: null
+  }
+
   componentDidMount(){
     const { navigation } = this.props;
     getWebToken()
@@ -20,16 +28,55 @@ class Login extends React.Component {
       })
       .then(res => res.json())
       .then(data => {
-        console.log(data)
         if(data.user_name){
           navigation.navigate(
             'HomeTabs'
           )
         }
       });
-  })
-  .catch(err => console.log(err));
-}
+    })
+    .catch(err => console.log(err));
+  }
+
+  onSubmit = () => {
+    const { navigation } = this.props;
+    const { user_name, password } = this.state;
+    const payload = {
+      user_name: user_name,
+      password: password,
+    }
+    fetch(`${BASE_URL}/login`, { 
+      headers: { 
+        'content-type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(payload)
+    })
+    .then(processResponse)
+    .then(res => {
+      const { data } = res;
+      if(data.token){
+        setWebToken(data.token)
+        this.setState({
+          userError: null,
+          passwordError: null
+        })
+        navigation.navigate(
+          'HomeTabs'
+        )
+      }
+      if(data.error){
+        this.setState({
+          userError: data.error.usernameError,
+          passwordError: data.error.passwordError
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      throw err
+    })
+  }
 
   render() {
     return (
@@ -40,18 +87,32 @@ class Login extends React.Component {
               placeholder="Username" 
               placeholderTextColor={white}
               style={{color: white}}
+              onChangeText={(user_name) => this.setState({user_name})}
             />
           </Item>
+          {
+            this.state.userError &&
+            <View style={styles.errorContainer}>
+              <Text style={styles.errorMessage}>{this.state.userError}</Text>
+            </View>
+          }
           <Item style={styles.inputContainer} stackedLabel underline>
             <Input
               placeholder="Password"
               secureTextEntry={true}
               placeholderTextColor={white}
               style={{color: white}}
+              onChangeText={(password) => this.setState({password})}
             />
           </Item>
+          {
+            this.state.passwordError &&
+            <View style={styles.errorContainer}>
+              <Text style={styles.errorMessage}>{this.state.passwordError}</Text>
+            </View>
+          }
         </Form>
-        <Button style={styles.button} block>
+        <Button style={styles.button} block onPress={this.onSubmit}>
           <Text>Login!</Text>
         </Button>
       </KeyboardAvoidingView>
@@ -73,6 +134,12 @@ const styles = StyleSheet.create({
   button: {
     margin: 20,
     backgroundColor: darkBlue
+  },
+  errorContainer: {
+    marginLeft: 20
+  },
+  errorMessage: {
+    color: red
   }
 })
 
